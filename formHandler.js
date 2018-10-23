c8ead187b36862e96bf8f7d110b65db8d9c7798b
Punisa <dasha888999@mail.ru>
2018-10-23 16:37:22 +0300

    tuk

--- formHandler.js
+++ formHandler.js
@@ -1,20 +1,31 @@
-function send() {
-    let xml = new XMLHttpRequest();
+console.log(document.forms);
 
-    var dataForm = new FormData(document.forms.test_form);
-    var xhr = new XMLHttpRequest(); //создаем объект
-    xhr.onreadystatechange = function() {
-           if (this.readyState == 4 && this.status == 200) { //проверяем ответ на ошибки 
-               // если норм то выводим
-               console.log(this.responseText) 
-           }
-           else
-           {
-               // если нет сообщаем об ошибке
-               console.log('error')
-           }	
-       };
-    xhr.open("POST", "/cont.php"); 
-    xhr.send(formData);
+function send(e) {
+    console.log(document.forms);
 
+    // let xml = new XMLHttpRequest();
+    // var dataForm = new FormData(document.forms.test_form);
+    // var xhr = new XMLHttpRequest(); //создаем объект
+    // xhr.onreadystatechange = function() {
+    //        if (this.readyState == 4 && this.status == 200) { //проверяем ответ на ошибки 
+    //            // если норм то выводим
+    //            console.log(this.responseText) 
+    //        }
+    //        else
+    //        {
+    //            // если нет сообщаем об ошибке
+    //            console.log('error')
+    //        }	
+    //    };
+    // xhr.open("POST", "/cont.php"); 
+    // xhr.send(formData);
+
+}
+
+let btn = document.getElementById('btn');
+
+
+btn.onclick = function(event) {
+    event.preventDefault();
+    console.log(document.forms);
 }
--- js/script.js
+++ js/script.js
@@ -18,41 +18,96 @@
 // 	}) 
 // })
 
-let Pad = {};
+// let Pad = {};
 
-Pad.name = "wacom";
-Pad.model = "CTL-471";
-Pad.DPI = 4096;
-Pad.draw = function () {
-	console.log('drawing...');
+// Pad.name = "wacom";
+// Pad.model = "CTL-471";
+// Pad.DPI = 4096;
+// Pad.draw = function () {
+// 	console.log('drawing...');
+// }
+
+// for( attr in Pad) {
+// 	console.log(Pad[attr]);
+// }
+// console.log('------------------------')
+
+// let PadF = function (name,model) {
+// 	this.name = name;
+// 	this.model = model;
+// 	this.draw = function () {
+// 		console.log('drawing...');
+// 	}
+// }
+
+
+// let wac = new PadF('wacom','CTL-471');
+// console.log(wac.name,wac.model);
+// wac.draw();
+// console.log('--------------');
+
+// let el = document.getElementById('nav-ul');
+// console.log(el);
+// console.log('--------------');
+// let els = document.getElementsByTagName('li');
+// console.log(els);
+// console.log('--------------');
+
+// for ( let el in els) {
+// 	console.log(els[el])
+// }
+
+//console.log(document.forms);
+
+
+let btn = document.getElementById('btn');
+let inp = document.getElementById('inp');
+
+
+btn.onclick = function(event) {
+	event.preventDefault();
+	console.log('click');
+	// console.log(document.forms.test_form.test.value);
+	// console.log('------------------------------------');
+	// console.log(document.forms['test_form']['test'].value);
+	// console.log('------------------------------------');
+	// let data = new FormData(document.forms.test_form);
+	// console.log(data);
+	// console.log(data.get("test"));
+	// console.log('------------------------------------');
 }
 
-for( attr in Pad) {
-	console.log(Pad[attr]);
-}
-console.log('------------------------')
-
-let PadF = function (name,model) {
-	this.name = name;
-	this.model = model;
-	this.draw = function () {
-		console.log('drawing...');
-	}
+btn.oncontextmenu = function() {
+	console.log('contextMenu');
 }
 
+btn.onmousedown = function() {
+	console.log('мышка нажата');
+}
+btn.onmouseup = function() {
+	console.log('мышка опущена');
+}
 
-let wac = new PadF('wacom','CTL-471');
-console.log(wac.name,wac.model);
-wac.draw();
-console.log('--------------');
+btn.onmouseleave = function() {
+	console.log('мышка уведена');
+}
 
-let el = document.getElementById('nav-ul');
-console.log(el);
-console.log('--------------');
-let els = document.getElementsByTagName('li');
-console.log(els);
-console.log('--------------');
+btn.onmouseenter = function() {
+	console.log('мышка Наведена');
+}
 
-for ( let el in els) {
-	console.log(els[el])
+inp.onkeydown = function() {
+	console.log('click');
+}
+
+inp.onfocus = function() {
+	console.log('focus');
+}
+
+inp.onblur = function() {
+	console.log('blur');
+}
+
+window.onload = function() {
+	console.log('window load');
 }
